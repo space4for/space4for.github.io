630a9fef40289b5da1ea2d00f4dcb8ec535bd081
righttime <eyedoloveu@gmail.com>
2016-04-05 01:03:47 +0900

    people button add event: "

--- assets/js/people.js
+++ assets/js/people.js
@@ -6,18 +6,22 @@ $(document).ready(function(){
 			case 0:
 				$("#btn_ljw").toggleClass('rotate90');
 				$("#ljh").toggleClass('hidden visible');
+				$(".people_bwimage:eq(0)").toggleClass('hidden visible');
 				selectedID = 1;
 			break;
 			case 1:
 				$("#btn_ljw").toggleClass('rotate90');
 				$("#ljh").toggleClass('hidden visible');
+				$(".people_bwimage:eq(0)").toggleClass('hidden visible');
 				selectedID = 0;
 			break;
 			case 2:
 				$("#btn_chj").toggleClass('rotate90');
 				$("#chj").toggleClass('hidden visible');
+				$(".people_bwimage:eq(1)").toggleClass('hidden visible');
 				$("#btn_ljw").toggleClass('rotate90');
 				$("#ljh").toggleClass('hidden visible');
+				$(".people_bwimage:eq(0)").toggleClass('hidden visible');
 				selectedID = 1;
 			break;
 		}
@@ -28,18 +32,22 @@ $(document).ready(function(){
 			case 0:
 				$("#btn_chj").toggleClass('rotate90');
 				$("#chj").toggleClass('hidden visible');
+				$(".people_bwimage:eq(1)").toggleClass('hidden visible');
 				selectedID = 2;
 			break;
 			case 1:
 				$("#btn_ljw").toggleClass('rotate90');
 				$("#ljh").toggleClass('hidden visible');
+				$(".people_bwimage:eq(0)").toggleClass('hidden visible');
 				$("#btn_chj").toggleClass('rotate90');
 				$("#chj").toggleClass('hidden visible');
+				$(".people_bwimage:eq(1)").toggleClass('hidden visible');
 				selectedID = 2;
 			break;
 			case 2:
 				$("#btn_chj").toggleClass('rotate90');
 				$("#chj").toggleClass('hidden visible');			
+				$(".people_bwimage:eq(1)").toggleClass('hidden visible');
 				selectedID = 0;
 			break;
 		}
